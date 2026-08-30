91d5fbd17e4227ed6753027f9cfc9edf0d1cddc6
DevJoaoLoureiro <joaoloureirosk8@gmail.com>
2026-08-30 08:57:35 +0100

    getprofile with browser cache

--- src/lib/auth/get-current-profile.ts
+++ src/lib/auth/get-current-profile.ts
@@ -1,6 +1,7 @@
+import { cache } from "react";
 import { createClient } from "@/lib/supabase/server";
 
-export async function getCurrentProfile() {
+export const getCurrentProfile = cache(async () => {
   const supabase = await createClient();
 
   // 1. Utilizador autenticado
@@ -83,4 +84,4 @@ export async function getCurrentProfile() {
     active: profile.active,
     store,
   };
-}
+});
